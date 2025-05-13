80dbd902e3afd4da7c1b04f83eb10b0c7471a39d
yttrion <pierreyvesdouault@gmail.com>
2025-05-13 15:43:37 +0200

    (feature/v2): Imported nvimtree configuration; removed noice.lua; Fixed syntax in plugin definition

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,14 +7,13 @@
 require("lazy").setup({
     spec = {
         -- GUI
-        { "nvim-lualine/lualine.nvim", lazy=true, dependencies = { "nvim-tree/nvim-web-devicons" }},
-        { "folke/noice.nvim", event="VeryLazy", dependencies={ "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" }},
+        { "nvim-lualine/lualine.nvim", lazy=true, dependencies={ "nvim-tree/nvim-web-devicons" }},
         { "akinsho/bufferline.nvim", event="VeryLazy" , version = "*", dependencies={ "nvim-tree/nvim-web-devicons" }},
         
         -- Tools
-        { "folke/which-key.nvim", lazy=true, dependencies = { "nvim-tree/nvim-web-devicons" }},
+        { "folke/which-key.nvim", lazy=true, dependencies={ "nvim-tree/nvim-web-devicons" }},
         { "dstein64/vim-startuptime", cmd="StartupTime" },
-        { "nvim-tree/nvim-tree.lua", lazy=true}, dependencies = { "nvim-tree/nvim-web-devicons" } },
+        { "nvim-tree/nvim-tree.lua", lazy=true, dependencies={ "nvim-tree/nvim-web-devicons" }},
 
     },
     install = { colorscheme = { "habamax" } },
@@ -24,4 +23,6 @@ require("lazy").setup({
 
 -- Call plugins configuration
 require("plugins.lualine")
-require("plugins.noice")
+require("plugins.bufferline")
+
+require("plugins.nvim-tree")
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,14 +1,29 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
 vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
 require("nvim-tree").setup({
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
 })
 
+local map = vim.keymap.set
+map("", "<leader>fe", ":NvimTreeToggle<CR>", { silent = true, desc = "File Explorer open" })
 
