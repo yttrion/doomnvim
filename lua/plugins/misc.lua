779853ab6d8959901acb1cdbb89351a67febd8d3
yttrion <pierreyvesdouault@gmail.com>
2025-05-14 11:32:44 +0200

    (feature/v2): Migrating to floaterm

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -6,5 +6,5 @@ require("bufferline").setup()
 require("smartcolumn").setup()
 
 -- Util keymaps
-map("", "<leader>hu", ":DistroUpdate<CR>",                                      { silent = true, desc = "Update doomnvim" }
-map("", "<leader>tf", ":FloatermToggle<CR>",                                    { silent = true, desc = "Toggle Floating term" }
+map("", "<leader>hu", ":DistroUpdate<CR>",                                      { silent = true, desc = "Update doomnvim" })
+map("", "<leader>tf", ":FloatermToggle<CR>",                                    { silent = true, desc = "Toggle Floating term" })
